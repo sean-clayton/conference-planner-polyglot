54f18cf24aaf972d09ac30d52f40b73a39472bc0
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2019-09-02 00:06:18 -0400

    Migration update

--- ConferencePlannerCS/ConferencePlanner/Startup.cs
+++ ConferencePlannerCS/ConferencePlanner/Startup.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ConferencePlanner.Models;
+using ConferencePlanner.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
